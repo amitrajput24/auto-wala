8d924095c0248ae6798aa303a6966e5925c2f4b0
amitrajput24 <rahulkuamar2100@gmail.com>
2026-08-11 10:16:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const upload=multer({storage,fileFilter:(req,f,cb)=>cb(null,/^audio\/|\.mp3$|\.w
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public"))); app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"index.html")));
 app.use("/music",express.static(MUSIC));
 app.use("/admin",express.static(path.join(__dirname,"admin")));
 
