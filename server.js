96a08f25a4b9b8ff6f11eabb4eb573d17751eec8
amitrajput24 <rahulkuamar2100@gmail.com>
2026-08-11 10:36:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require("express"); const path=require("path");
 const multer=require("multer");
 const fs=require("fs");
-const path=require("path");
+
 const app=express(); app.use("/admin",express.static(path.join(__dirname,"admin")));
 const PORT=process.env.PORT||3000;
 const ADMIN_PASSWORD=process.env.ADMIN_PASSWORD||"change-this-password";
